68fb40b64c50a7fd502ae5fdcb083149d7582e9c
anwesha-mercury <anwesha@mercurydasha.com>
2025-04-17 21:44:54 +0530

    Added vercel configs

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineConfig({
+export default defineConfig({ 
   plugins: [
     react(),
     runtimeErrorOverlay(),
